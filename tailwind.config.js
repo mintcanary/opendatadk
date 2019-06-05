41b35b015cb7267a349214942375532ce9fa66f6
Sam Smith <sam@mintcanary.com>
2019-06-05 11:07:25 +0100

    footer content

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       primary: '#321E41',
       secondary: '#FF3464',
       white: '#fff',
+      'white-75': 'rgba(255,255,255,0.75)',
       gray: {
         '100': '#F5F4F6',
         '500': '#6D6E71',
