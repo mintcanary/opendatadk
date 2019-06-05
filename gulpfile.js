3d4252bfc10cd930be5cf45377069431d4abb8c5
Sam Smith <sam@mintcanary.com>
2019-06-05 12:20:06 +0100

    add some icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
 const postcssPresetEnv = require('postcss-preset-env');
+const svgSprite = require('gulp-svg-sprite');
 
 // CSS
 gulp.task('css', function () {
@@ -17,13 +18,34 @@ gulp.task('css', function () {
     .pipe(gulp.dest('./css'));
 });
 
+
+// icons
+gulp.task('icons', function(done) {
+  gulp.src('**/*.svg', { cwd: './src/icons' })
+    .pipe(svgSprite(
+      config = {
+        mode: {
+          symbol: true
+        }
+      }
+    ))
+    .pipe(gulp.dest('./_includes'));
+    done();
+});
+
+
 // watching
 gulp.task('watch:css', function() {
   return gulp.watch(['./src/css/**/*.css'],
   gulp.series('css'));
 });
 
-gulp.task('watch', gulp.parallel('watch:css'));
+gulp.task('watch:icons', function() {
+  return gulp.watch(['./src/icons/**/*.svg'],
+  gulp.series('icons'));
+});
+
+gulp.task('watch', gulp.parallel('watch:css', 'watch:icons'));
 
 // deafult task
 gulp.task('default', gulp.parallel('watch'));
