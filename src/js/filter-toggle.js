b6f6b35ed50a237e8d8c5359c84382c155fc3f1a
Sam Smith <sam@mintcanary.com>
2019-06-05 17:20:33 +0100

    add toggle for filters

--- src/js/filter-toggle.js
+++ src/js/filter-toggle.js
@@ -13,16 +13,3 @@ filterToggle.onclick = function() {
   filterToggle.setAttribute('aria-expanded', !expanded);
   filterTarget.setAttribute("data-expanded", !expanded);
 }
-
-// replace link with a button
-var searchLink = document.getElementById("search-link");
-var searchButton = document.createElement("button");
-searchButton.setAttribute("id", "show-search");
-
-while (searchLink.childNodes.length > 0) {
-  searchButton.appendChild(searchLink.childNodes[0]);
-}
-
-searchLink.replaceWith(searchButton);
-
-// TODO: implement a modal
